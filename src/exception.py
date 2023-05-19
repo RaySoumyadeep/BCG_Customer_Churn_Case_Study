ec1415da71f95c08fb7bba3af75fb8d47347f7bf
RaySoumyadeep <soumyadeep.ray@okstate.edu>
2023-05-19 15:47:00 -0500

    data and notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb= error_detail.exc_info()  #This function returns a tuple containing the exception type,  the exception itself, and a traceback object which typically encapsultates the call stack at the point where the exception last occured.
@@ -20,11 +20,11 @@ class CustomException(Exception):
         '''
         return self.error_message
            
-if __name__ == '__main__':
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Error divide by zero")
-        raise CustomException(e,sys)
+    # if __name__ == '__main__':
+    #     try:
+    #         a=1/0
+    #     except Exception as e:
+    #         logging.info("Error divide by zero")
+    #         raise CustomException(e,sys)
         
 
